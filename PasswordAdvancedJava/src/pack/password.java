942eaa6e9c15685d6dfd19b6173ecdace4a32701
CharlotteH86 <70633528+CharlotteH86@users.noreply.github.com>
2021-09-07 12:06:49 +0200

    added code

---
+++ PasswordAdvancedJava/src/pack/password.java
@@ -0,0 +1,41 @@
+package pack;
+
+import java.util.Scanner;
+
+public class password {
+
+	public static void main(String[] args) {
+
+		Scanner text = new Scanner(System.in);
+
+		String password = "abc123";
+
+		int attempts = 0;
+
+		System.out.println("Enter password: ");
+
+		String inputPassword;
+
+		while (true) {
+			inputPassword = text.nextLine();
+
+			if (inputPassword.equals(password)) {
+				System.out.println("Right Password!");
+				break;
+
+			} else if (inputPassword != password) {
+
+				System.out.println("Wrong try again");
+
+			}
+			if (attempts == 2) {
+				System.out.println("Account locked");
+				break;
+
+			}
+			attempts++;
+
+		}
+	}
+
+}
